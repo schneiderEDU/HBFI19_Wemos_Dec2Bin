a8e0fc9e5693405a1bcd0c182dbc018007af09a1
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2020-05-11 15:21:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
 #include <Arduino.h>
 
 int dec = 25; //11001
-bool isCalculated = false;
 
-String calcWhile(int dec);
+
+String toBin(int dec);
 
 void setup() {
   Serial.begin(9600);
+  Serial.printf("\ndec = %i\n", dec);
+  Serial.print("bin = ");
+  Serial.println(toBin(dec));
 }
 
 void loop() {
-  if(!isCalculated) {    
-    Serial.printf("\ndec = %i\n", dec);
-    Serial.print("bin = ");
-    Serial.println(calcWhile(dec));
-  }
+
 }
 
-String calcWhile (int dec) {
+String toBin (int dec) {
   String binary = "";
   while(dec > 0) {
     int rest = dec%2;
     binary = rest + binary;    
     dec /= 2;
   }
-  isCalculated = true;
   return binary;
 }
